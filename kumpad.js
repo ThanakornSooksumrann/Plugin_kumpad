7ffe7ed822473f9fa98fda7c62b9cc8adfc3290d
Thanakorn <52211491+ThanakornSooksumrann@users.noreply.github.com>
2023-08-10 14:48:47 +0700

    Update kumpad.js

--- kumpad.js
+++ kumpad.js
@@ -1,16 +1,2 @@
 // plugin ทำให้ element เท่ากัน
-(function($) {
-    $.fn.equalizeHeight = function() {
-        return this.each(function() {
-            var $elements = $(this).find('[data-equalize="height"]');
-            var maxHeight = 0;
-            $elements.each(function() {
-                var elementHeight = $(this).outerHeight();
-                if (elementHeight > maxHeight) {
-                    maxHeight = elementHeight;
-                }
-            });
-            $elements.outerHeight(maxHeight);
-        });
-    };
-})(jQuery);
+
